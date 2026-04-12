256765704096848e812ebc32b9dc780cf9e9036c
Yashuu05 <chillalyash2005@gmail.com>
2026-04-12 14:12:09 +0530

    updated frontend removing signup page

--- app.py
+++ app.py
@@ -3,22 +3,8 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
-
-@app.route("/signup")
-def signup():
-    return render_template("signup.html")
-
-@app.route("/api/signup", methods=["POST"])
-def api_signup():
-    """Future: Handle user signup logic"""
-    return {"message": "Signup endpoint - to be implemented"}, 200
-
-@app.route("/api/signin", methods=["POST"])
-def api_signin():
-    """Future: Handle user signin logic"""
-    return {"message": "Signin endpoint - to be implemented"}, 200
 
 if __name__ == "__main__":
     app.run(debug=True)
